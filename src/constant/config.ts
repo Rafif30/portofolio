da2c9cb37e91937a70c7aecfb2ee6e21c0cd4a5c
Rafif30 <imamrafif25@gmail.com>
2025-11-22 16:41:09 +0700

    fix: change variable name

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,3 +1,3 @@
-export const EMAIL_USER = process.env.NEXT_PUBLIC_AUTH_EMAIL_USER;
-export const EMAIL_PASS = process.env.NEXT_PUBLIC_AUTH_EMAIL_PASS;
-export const RECEIVER_EMAIL = process.env.NEXT_PUBLIC_AUTH_RECEIVER_EMAIL;
+export const EMAIL_USER = process.env.NEXT_AUTH_EMAIL_USER;
+export const EMAIL_PASS = process.env.NEXT_AUTH_EMAIL_PASS;
+export const RECEIVER_EMAIL = process.env.NEXT_AUTH_RECEIVER_EMAIL;
